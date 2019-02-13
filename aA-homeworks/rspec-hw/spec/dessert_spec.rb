d248b660d5523241f9d8c1aa3ced3dfe407e441a
pvulakh <pvulakh@gmail.com>
2019-02-12 23:56:19 -0500

    Finish Dessert specs

--- aA-homeworks/rspec-hw/spec/dessert_spec.rb
+++ aA-homeworks/rspec-hw/spec/dessert_spec.rb
@@ -29,22 +29,36 @@ describe Dessert do
       expect(cake.add_ingredient("flour")).to eq(["flour"])
     end
   end
-
+  
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do 
+      ingredients = ["flour", "egg", "milk", "butter", "sugar"]
+      ingredients.each { |ingredient| cake.add_ingredient(ingredient) }
+      expect(cake.ingredients).to eq(ingredients)
+      cake.mix!
+      expect(cake.ingredients).to_not eq(ingredients)
+    end 
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
-
-    it "raises an error if the amount is greater than the quantity"
+    it "subtracts an amount from the quantity" do 
+      expect(cake.eat(1)).to eq(0)
+    end 
+    it "raises an error if the amount is greater than the quantity" do 
+      expect { cake.eat(100)}.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do 
+      allow(chef).to receive(:titleize).and_return("Chef Chef the Great Baker")
+      expect(cake.serve).to eq("Chef Chef the Great Baker has made 1 cakes!")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do 
+      allow(chef).to receive(:bake)
+    end
   end
 end
