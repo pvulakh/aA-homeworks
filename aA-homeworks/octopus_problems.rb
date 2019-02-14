44365626b6d9eedc9f52e8e55d5a5df6a9f9b39d
pvulakh <pvulakh@gmail.com>
2019-02-13 20:34:59 -0500

    write sluggish octopus

---
+++ aA-homeworks/octopus_problems.rb
@@ -0,0 +1,8 @@
+FISH = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish', 
+        'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
+
+def slug_oct
+  FISH.each do |fish1, idx1|
+    FISH.each do |fish1
+
+end 
