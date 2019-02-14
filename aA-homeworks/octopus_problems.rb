4f1ae4665bf4b9efa120145f90017588f2d5180d
pvulakh <pvulakh@gmail.com>
2019-02-13 20:51:02 -0500

    add #slow_dance, #fast_dance: O(n), O(1) search

--- aA-homeworks/octopus_problems.rb
+++ aA-homeworks/octopus_problems.rb
@@ -46,3 +46,21 @@ def clever_oct
   FISH.each { |fish| max_fish = fish if fish.length > max_fish.length }
   max_fish
 end 
+
+TILES = ["up", "right-up", "right", "right-down", "down", "left-down", "left",  "left-up" ]
+
+def slow_dance(tile, tiles_array)
+  tiles_array.each { |dir| return tiles_array.index(dir) if dir == tile }
+end
+
+FAST_TILES = { "up" => 0, "right-up" => 1, "right" => 2, "right-down" => 3,
+ "down" => 4, "left-down" => 5, "left" => 6,  "left-up" => 7 }
+
+
+def fast_dance(tile, tiles_array)
+  FAST_TILES[tile]
+end
+
+
+
+
