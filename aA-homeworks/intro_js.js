ba7158e67c669415150737713ba3a41e85efbf60
pvulakh <pvulakh@gmail.com>
2019-03-09 13:08:24 -0500

    Finish Intro JS Problems part 1

--- aA-homeworks/intro_js.js
+++ aA-homeworks/intro_js.js
@@ -1,6 +1,6 @@
 function madLib(verb, adj, noun) {
   console.log(`We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${ noun.toUpperCase()}`);
-};
+}
 
 function isSubstring(searchString, subString) {
   if (searchString.includes(subString)) {
@@ -8,12 +8,49 @@ function isSubstring(searchString, subString) {
   } else {
     return false;
   }
-};
+}
 
 function fizzBuzz(array) {
-  array.forEach(function(el)) {
-    if (el % 3 || el % 5) {
-      return [el];
-    };
+  let result = [];
+
+  array.forEach(function(el) {
+    if ((el % 3  == 0 && el % 5 != 0 ) || (el % 5 == 0 && el % 3 != 0)) {
+      result.push(el);
+    }
+  });
+  return result;
+}
+
+function isPrime(num) {
+  if (num < 2) {
+    return false;
   }
-};
+
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+ }
+
+
+ function sumOfNPrimes(n) {
+   let sum = 0;
+
+   let i = 0;
+   let num = 1;
+
+   while (i < n) {
+     if (isPrime(num)) {
+       sum += num;
+       num++;
+       i++;
+     } else {
+       num ++;
+     }
+   }
+
+   return sum;
+ }
