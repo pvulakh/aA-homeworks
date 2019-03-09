b0cb2a9de58aae60db90cc3d4e4529452c8aaba7
pvulakh <pvulakh@gmail.com>
2019-03-09 13:48:01 -0500

    Patch Elephant Constructor to have parade functionality

--- aA-homeworks/intro_js.js
+++ aA-homeworks/intro_js.js
@@ -91,3 +91,17 @@ function isPrime(num) {
      console.log(`${this.name} is ${randTrick}`);
    }
  }
+
+Elephant.paradeHelper = function (elephant) {
+  console.log(`${elephant.name} is trotting by!`);
+};
+
+
+// let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+// let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water"]);
+// let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+// let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+// let herd = [ellie, charlie, kate, micah];
+
+// herd.forEach(Elephant.paradeHelper);
