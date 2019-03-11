df5b27b08e709577fe3fc1fd9e0b397693438116
pvulakh <pvulakh@gmail.com>
2019-03-10 22:33:38 -0400

    Draw rectangle and circle

--- aA-homeworks/canvas_practice/practice.js
+++ aA-homeworks/canvas_practice/practice.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById('mycanvas');
+  const ctx = canvas.getContext('2d');
 
+  ctx.fillStyle = 'green';
+  ctx.fillRect(10, 10, 100, 100);
+
+ ctx.beginPath();
+ ctx.arc(200, 200, 50, 0, 2 * Math.PI);
+ ctx.strokeStyle = 'blue';
+ ctx.lineWidth = 1;
+ ctx.stroke();
+ 
+ ctx.fillStyle = "red";
+ ctx.fill();
 });
